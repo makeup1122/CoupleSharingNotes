8b14bf75e33050add87159caa5a7958060e6aa7e
makeup1122 <makeup1123@163.com>
2019-01-05 00:18:12 +0800

    删除过渡动画

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <!-- <v-container fluid class="pa-0"> -->
       <v-layout column>
         <v-flex>
-          <transition name="bounce">
+          <transition name="slide-left">
             <router-view></router-view>
           </transition>
         </v-flex>
@@ -26,21 +26,5 @@ export default {
 }
 </script>
 <style>
-.bounce-enter-active {
-  animation: bounce-in .5s;
-}
-.bounce-leave-active {
-  animation: bounce-in .5s reverse;
-}
-@keyframes bounce-in {
-  0% {
-    transform: scale(0);
-  }
-  50% {
-    transform: scale(1.5);
-  }
-  100% {
-    transform: scale(1);
-  }
-}
+
 </style>
